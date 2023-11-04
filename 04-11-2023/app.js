556933999046c0b5332b6fce868bd09bd4371949
TechMaestro01 <asifali0160@gmail.com>
2023-11-04 23:13:52 +0500

    JavaScript assignment 01 work
    JavaScript assignment 01 is completed

---
+++ 04-11-2023/app.js
@@ -0,0 +1,47 @@
+// Question no 1
+var name = "Hello World";
+alert(name);
+
+// Question no 2
+// camel case (easy to read and use mostly world wide as international standard)
+var first = "Welcome to SMIT";
+var last = "\nA Place to change your Life";
+var myFullName = first + " " + last ;
+alert (myFullName);
+
+// Question no 3
+// camel case (easy to read and use mostly world wide as international standard)
+var first = "Camel";
+var last = "Case";
+var myFullName = first + " " + last ;
+alert (myFullName);
+
+// Question no 4
+alert ("My email address is asifali@gmail.com")
+
+// Question no 5
+var name = "Saylani\nsaylan\nSayla\nSayl\nSay\nSa\nS";
+alert (name);
+
+// Question no 6
+alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// Question no 7
+// concrenation
+var num = "10" ;
+var num = num + 5 ;
+var name = "Concrenation"
+alert (num + " " + name);
+
+// Question no 8
+// sanke case
+var first = "Sanke";
+var last = "Case";
+var full_name = first + " " + last ;
+alert (full_name);
+
+// Question no 9
+// Rules for naming JS Variables
+var name = "______"
+
+
